2492489878c2173a0d33d9b3b3a70c1e8bdd3403
Kostyantyn Kalynovskyi <kkalynovskyi@mirantis.com>
2017-03-02 16:40:47 +0200

    first working copy

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM ubuntu:16.04
 LABEL owner=kkalynovskyi@mirantis.com
+ENV DEBIAN_FRONTEND=noninteractive
 ARG KEYURL="https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub"
 ARG REPOSTRING="deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main"
 ARG SALTPACKAGES="salt-master salt-cloud"
-RUN apt-get update -q && apt-get install -y apt-utils wget
+RUN apt-get update -q && apt-get install -q -y apt-utils wget
 RUN wget -O - $KEYURL | apt-key add -
 RUN echo $REPOSTRING > /etc/apt/sources.list.d/saltstack.list
-RUN apt-get update -q && apt-get install -y $SALTPACKAGES
+RUN apt-get update -q && apt-get install -y -q $SALTPACKAGES
 COPY deployment/salt/etc /etc/salt
-ENTRYPOINT /usr/bin/python /usr/bin/salt-master
-RUN salt-cloud --assume-yes --parallel -m /etc/salt/minion_map
+ENTRYPOINT ["/usr/bin/python", "/usr/bin/salt-master"]
 
--- my_ssh_vagrant.sh
+++ my_ssh_vagrant.sh
@@ -1,10 +1,7 @@
-vagrant up --provider libvirt
-sleep 10
-minions=$(vagrant status | awk '/running/ {print $1}')
 key_name="id_rsa"
 salt_conf_dir="deployment/salt/etc"
 key_file_path="/etc/salt/${key_name}"
-SALT_MASTER_IP=$(grep base_ip_net deployment_model.yaml | tr -d "[:space:],\",\',base_ip_net:").1
+SALT_MASTER_IP=$(grep '^  base_ip_net:' deployment_model.yaml | tr -d "[:space:],\",\',base_ip_net:").1
 
 __distribute_key() {
 
@@ -18,7 +15,7 @@ generate_key_pair(){
   local local_key_path="${salt_conf_dir}/${key_name}"
   local local_public_key_path="${salt_conf_dir}/${key_name}.pub"
   echo "generating key-pair"
-  if [ ! -f /tmp/foo.txt ]; then
+  if [ ! -f $local_key_path ]; then
     ssh-keygen -t rsa -N "" -f ${local_key_path}
   else
     echo "key at ${local_key_path} already exists, skipping"
@@ -34,6 +31,7 @@ parse_and_deploy_keys() {
   generate_key_pair public_key
   echo "saltification:" > $map_file_path
   echo "starting to distribute ssh_keys, minions are: \\n $minions"
+  local minions=$(vagrant status | awk '/running/ {print $1}')
   for minion in $minions; do
     echo "distributing key to minion: $minion"
     ssh_conf=$(vagrant ssh-config "${minion}")
@@ -52,26 +50,25 @@ parse_and_deploy_keys() {
 
 }
 
-configure_master(){
-
-  echo "setting minion connfiguration for salification"
-  sed -i "s/<master_ip_address>/${SALT_MASTER_IP}/" deployment/salt/etc/cloud.provider
-  sed -i "s/<key_name>/${key_name}/" deployment/salt/etc/minion_map
-}
 
 build_and_run_master(){
   local master_image_name=salt-master-${SALT_MASTER_IP}
   local master_container_name=salt-master-${SALT_MASTER_IP}
 
   if [[ "$(sudo docker images | grep -v $master_image_name)" ]]; then
-    sudo docker build --tag ${master_image_name} .
+    echo "Starting to build image ${master_image_name}"
+    cmd="sudo docker build --tag ${master_image_name} ."
+    echo $cmd
+    $cmd 
   fi
   if [[ "$(sudo docker ps | grep -v ${master_container_name})" ]]; then
     sudo docker run -it -d --network=host --name=${master_container_name} ${master_image_name}
+    sudo docker exec ${master_container_name} /usr/bin/python /usr/bin/salt-cloud --assume-yes --parallel -m /etc/salt/minion_map 
   fi
 
 }
 
+vagrant up --provider libvirt
+sleep 10
 parse_and_deploy_keys
-configure_master
 build_and_run_master
